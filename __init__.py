4e053ef0fd9047f0321cc9c7e2e9c4f7f7a0d0a8
Eugene R. Miller <theerm@gmail.com>
2016-06-25 03:17:30 -0600

    oops wrong header

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
-# Copyright 2016 Mycroft AI, Inc.
+# Copyright 2016 Eugene R. Miller
 #
-# This file is part of Mycroft Core.
-#
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# Mycroft Jupiter Broadcasting skill is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# Mycroft Jupiter Broadcasting skill is distributed in the hope that it will
+#  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+# along with Mycroft Jupiter Broadcasting skill.
+# If not, see <http://www.gnu.org/licenses/>.
 
 from os.path import dirname, join, expanduser
 from adapt.intent import IntentBuilder
